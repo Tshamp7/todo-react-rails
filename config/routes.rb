72c56339be267a5fe5d9dd6da13cd712a979b0b7
Thomas Shamp <T.shamp7@gmail.com>
2021-01-22 13:00:04 -0800

    add API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'todos/index'
+  # get 'todos/create'
+  # get 'todos/update'
+  # get 'todos/destroy'
+
+  scope '/api/v1' do
+    resources :todos
+  end
 end
